476bf1c5a919b87f6fbb788bb70fb4900006e2ea
hungacmaa <hungkojno1@gmail.com>
2020-11-11 18:44:04 +0700

    hungcoi

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php
+ 
+// Require database & thông tin chung
+require_once 'core/init.php';
+ 
+// Require header
+require_once 'includes/header.php';
+ // Nếu đăng nhập
+if ($user)
+{
+ 
+}
+// Nếu không đăng nhập
+else
+{
+    // Hiển thị form đăng nhập
+    require_once 'templates/signin.php';
+}
+// Require footer
+require_once 'includes/footer.php';
+ 
+?>
